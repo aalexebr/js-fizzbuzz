9e5fe4be0e5e31908f732eac7bf5a4eccf438f6b
aalexebr <alex.ebr@icloud.com>
2023-06-14 16:59:14 +0200

    complete Bonus

--- js/script.js
+++ js/script.js
@@ -3,16 +3,14 @@
 
 
 
-for (let n = 1; n <= 15; n++){
+for (let n = 1; n <= 30; n++){
     
     let box = document.createElement('div')
     box.className = 'col'
     document.querySelector('.row').append(box)
+    
     box.innerText = n
-    
-    
 
-    
     // let text = document.createElement("h1");
     // text.innerText = n;
     // document.querySelector('.col').append(text)
@@ -21,14 +19,17 @@ for (let n = 1; n <= 15; n++){
     
     if (n % 3 == 0 && n % 5 == 0){
         box.innerText = 'FizzBuzz'
+        box.classList.add('fizzbuzz')
         console.log('nnumber is divided by both', n)
     }
     else if (n % 3 == 0){
         box.innerText = 'Fizz'
+        box.classList.add('fizz')
         console.log('nnumber is divided by 3', n)
     }
     else if (n % 5 == 0){
         box.innerText = 'Buzz'
+        box.classList.add('buzz')
         console.log('nnumber is divided by 5', n)
     }
 }
