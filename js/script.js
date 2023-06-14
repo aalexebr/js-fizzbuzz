d7e149a5214e3a9d7b04dfde026d91b35d00cc7b
aalexebr <alex.ebr@icloud.com>
2023-06-14 15:15:30 +0200

    added conditions of dividends

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-for (let n = 0; n <= 10; n++){
+for (let n = 1; n <= 15; n++){
     console.log('nnumber', n)
+    if (n % 3 == 0 && n % 5 == 0){
+        console.log('nnumber is divided by both', n)
+    }
+    else if (n % 3 == 0){
+        console.log('nnumber is divided by 3', n)
+    }
+    else if (n % 5 == 0){
+        console.log('nnumber is divided by 5', n)
+    }
 }
