1a9b6344b9e03374cc78f8748f662c5cd49d97f4
aalexebr <alex.ebr@icloud.com>
2023-06-14 16:28:45 +0200

    progressssssss

--- js/script.js
+++ js/script.js
@@ -4,27 +4,31 @@
 
 
 for (let n = 1; n <= 15; n++){
+    
     let box = document.createElement('div')
     box.className = 'col'
     document.querySelector('.row').append(box)
-    box = n
+    box.innerText = n
+    
+    
 
+    
     // let text = document.createElement("h1");
     // text.innerText = n;
-    // document.querySelector('.container').append(text)
+    // document.querySelector('.col').append(text)
     // document.body.appendChild(text);
     console.log('nnumber', n)
     
     if (n % 3 == 0 && n % 5 == 0){
-        // text.innerText = 'FizzBuzz';
+        box.innerText = 'FizzBuzz'
         console.log('nnumber is divided by both', n)
     }
     else if (n % 3 == 0){
-        // text.innerText = 'Fizz';
+        box.innerText = 'Fizz'
         console.log('nnumber is divided by 3', n)
     }
     else if (n % 5 == 0){
-        // text.innerText = 'Buzz';
+        box.innerText = 'Buzz'
         console.log('nnumber is divided by 5', n)
     }
 }
