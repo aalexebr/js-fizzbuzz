4a2b59b19bdd114547a5cf6e1e793e0eaf4923a5
aalexebr <alex.ebr@icloud.com>
2023-06-14 17:17:02 +0200

    added comments and notes

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@
 
 
 for (let n = 1; n <= 100; n++){
-    
+    // creates an element with a css class already defined after an html element .row
     let box = document.createElement('div')
     box.className = 'col'
     document.querySelector('.row').append(box)
-    
-    box.innerText = n
 
-    
+    // puts in the text number of the iteration in the created div
+    box.innerText = n
+  
+    // math conditions for the iterations
+    // begin with the unique AND condition and proceed with the secondary
+    // if the AND is last the iteration stops after the first if
     if (n % 3 == 0 && n % 5 == 0){
         box.innerText = 'FizzBuzz'
         box.classList.add('fizzbuzz')
